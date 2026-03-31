259a74a5907203bd7f80adaf9ba3c102e66b02ee
davi <lum@lum>
2026-03-30 21:35:17 -0300

    completed luo2011

--- tests/luo2011.py
+++ tests/luo2011.py
@@ -8,6 +8,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # ======================================================== #
 
+from dataclasses import dataclass
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,28 +21,28 @@ uncalled = True
 
 class SmoothCircle:
   def __init__(
-    self, obj: ObjectiveFunctions, nz: int, 
-    nx: int, center: tuple, value: int, 
-    ref_sigma: int, ref_amp: float
+    self, cfg: Parameters
   ) -> None:
 
-    self.obj = obj
+    self.cfg = cfg
 
-    self.nz, self.nx = nz, nx
+    self.nz, self.nx = cfg.nz, cfg.nx
 
-    self.center = center
-    self.ref_sigma = ref_sigma
-    self.ref_amp = ref_amp
+    self.center = cfg.center
+    self.ref_sigma = cfg.ref_sigma
+    self.ref_amp = cfg.ref_amp
 
-    self.value = value
+    self.value = cfg.value
 
-    self.ref_circle_smooth = np.zeros((nz, nx))
+    self.ref_circle_smooth = np.zeros((cfg.nz, cfg.nx))
 
     self.delta_sigma = np.array([])
     self.delta_amp = np.array([])
 
     self.l2 = np.array([])
     self.l1 = np.array([])
+
+    self.X, self.Y = None, None
 
   @measure_runtime
   def varying_circles(self, ratio: float, prop_type: str, iterations=11) -> None:
@@ -65,8 +67,8 @@ class SmoothCircle:
 
         self.delta_sigma[idx] = self.ref_sigma - sigma
 
-        self.l2[idx] = self.obj.l2_norm(self.ref_circle_smooth, d_calc)
-        self.l1[idx] = self.obj.l1_norm(self.ref_circle_smooth, d_calc)
+        self.l2[idx] = self.l2_norm(self.ref_circle_smooth, d_calc)
+        self.l1[idx] = self.l1_norm(self.ref_circle_smooth, d_calc)
 
     elif prop_type == "amp":
 
@@ -79,29 +81,29 @@ class SmoothCircle:
 
         self.delta_amp[idx] = self.ref_amp - amp
 
-        self.l2[idx] = self.obj.l2_norm(self.ref_circle_smooth, d_calc)
-        self.l1[idx] = self.obj.l1_norm(self.ref_circle_smooth, d_calc)
+        self.l2[idx] = self.l2_norm(self.ref_circle_smooth, d_calc)
+        self.l1[idx] = self.l1_norm(self.ref_circle_smooth, d_calc)
 
     elif prop_type == "both":
       
       for i, it in enumerate(range(-half_it, half_it + 1)):
-        self.delta_sigma[i] = it * ratio
-        self.delta_amp[i] = it * ratio
+        self.delta_sigma[i] = self.ref_sigma - it * ratio
+        self.delta_amp[i] = self.ref_amp - it * ratio
 
-      X, Y = np.meshgrid(self.delta_sigma, self.delta_amp)
+      self.X, self.Y = np.meshgrid(self.delta_sigma, self.delta_amp)
 
-      self.l2 = np.zeros_like(X)
-      self.l1 = np.zeros_like(X)
+      self.l2 = np.zeros_like(self.X)
+      self.l1 = np.zeros_like(self.Y)
 
       for i in range(iterations):
-        for j in range(iterations):
-          sigma = self.ref_sigma - X[i, j]
-          amp   = self.ref_amp   - Y[i, j]
-
-          d_calc = self.__smooth_kernel(sigma=sigma, amp=amp)
-
-          self.l2[i, j] = self.obj.l2_norm(self.ref_circle_smooth, d_calc)
-          self.l1[i, j] = self.obj.l1_norm(self.ref_circle_smooth, d_calc)
+       for j in range(iterations):
+         sigma = self.X[i, j]
+         amp   = self.Y[i, j]
+      
+         d_calc = self.__smooth_kernel(sigma=sigma, amp=amp)
+      
+         self.l2[i, j] = self.l2_norm(self.ref_circle_smooth, d_calc)
+         self.l1[i, j] = self.l1_norm(self.ref_circle_smooth, d_calc)
 
     else:
       raise TypeError("Make sure you choose a valid type. [sigma, amp, both]")
@@ -132,6 +134,12 @@ class SmoothCircle:
 
     return (1 + gaussian) * model
 
+  def l2_norm(self, d_obs, d_calc) -> np.ndarray:
+    return np.sqrt(np.sum((d_obs - d_calc)**2))
+
+  def l1_norm(self, d_obs, d_calc) -> np.ndarray:
+    return np.sum(np.abs(d_obs - d_calc))
+
   def plot(self, model=None) -> None:
     if model is None:
       model = self.ref_circle_smooth
@@ -152,12 +160,12 @@ class SmoothCircle:
 
     plt.show()
 
-  def plot_obj(self, obj_type: str, prop_type: str) -> None:
+  def plot_cfg(self, cfg_type: str, prop_type: str) -> None:
 
-    if obj_type == "L2":
-      obj = self.l2
-    elif obj_type == "L1":
-      obj= self.l1
+    if cfg_type == "L2":
+      cfg = self.l2
+    elif cfg_type == "L1":
+      cfg= self.l1
     else:
       raise TypeError("Make sure you choose a valid type. [L2, L1]")
 
@@ -170,55 +178,61 @@ class SmoothCircle:
 
     fig, ax = plt.subplots(figsize=(12, 5))
 
-    img = ax.plot(x, obj)
+    img = ax.plot(x, cfg)
 
     ax.set_xlabel(label, fontsize=13)
-    ax.set_ylabel(obj_type, fontsize=13)
+    ax.set_ylabel(cfg_type, fontsize=13)
     #ax.set_title("")
 
     plt.grid(True)
 
     plt.show()
 
-class ObjectiveFunctions:
+@dataclass
+class Parameters:
+  nz: int = 300
+  nx: int = 900
 
-  def l2_norm(self, d_obs, d_calc) -> np.ndarray:
-    return np.sqrt(np.sum((d_obs - d_calc)**2))
+  value: int = 2500.0
 
-  def l1_norm(self, d_obs, d_calc) -> np.ndarray:
-    return np.sum(np.abs(d_obs - d_calc))
+  center: tuple = (nz // 2, nx // 2)
 
-nz, nx = 300, 900
+  ref_sigma: int = 40
+  ref_amp: float = 0.40
 
-value = 2500.0
+  prop_type: str = "both"
 
-center = (nz // 2, nx // 2)
+cfg = Parameters()
 
-ref_sigma = 40
-ref_amp = 0.40
-
-prop_type = "both"
-
-obj = ObjectiveFunctions()
-
-smooth_circle = SmoothCircle(obj, nz, nx, center, value, ref_sigma, ref_amp)
+smooth_circle = SmoothCircle(cfg)
 smooth_circle.get_ref_circle()
-smooth_circle.varying_circles(ratio=0.3, prop_type=prop_type, iterations=11)
+smooth_circle.varying_circles(ratio=0.3, prop_type=cfg.prop_type, iterations=11)
 
 #smooth_circle.plot()
-#smooth_circle.plot_obj(obj_type="L1", prop_type=prop_type)
+#smooth_circle.plot_cfg(cfg_type="L1", prop_type=cfg.prop_type)
 
-try:
-  fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+fig, ax = plt.subplots(
+  figsize=(10, 8),
+  subplot_kw={"projection": "3d"}
+)
 
-  surf = ax.plot_surface(
-    smooth_circle.delta_sigma, 
-    smooth_circle.delta_amp, 
-    smooth_circle.l2, 
-    linewidth=0, 
-    antialiased=False
-  )
+surf = ax.plot_surface(
+    smooth_circle.X, 
+    smooth_circle.Y, 
+    smooth_circle.l2,
+    cmap="viridis",
+    linewidth=0,
+    antialiased=True
+)
 
-  plt.show()
-except:
-  pass
+fig.colorbar(surf, shrink=0.7, aspect=20)
+
+ax.set_box_aspect([1,1,1])
+
+ax.set_xlabel(r'$\Delta \sigma$')
+ax.set_ylabel(r'$\Delta amp$')
+ax.set_zlabel('L2')
+
+ax.view_init(elev=30, azim=45)
+
+plt.show()
